d06cb37df1231ae4fe4e29383c259e213fc4e287
Souvik Mishra <mishra.souvik911@gmail.com>
2023-10-31 12:22:59 +0530

    add new logo

--- src/lib/components/header/header.svelte
+++ src/lib/components/header/header.svelte
@@ -2,9 +2,11 @@
 	import ConnectButton from '../wallet/ConnectButton.svelte';
 </script>
 
-<div class="navbar bg-base-200 my-6 flex justify-between items-center h-[92px] rounded-box">
-	<div class="ml-6 flex-1">
+<div class="navbar p-0 bg-base-200 my-6 flex justify-between items-center h-[92px] rounded-box">
+	<div class="ml-8 flex items-center">
 		<a class="" href="/"> <img src="/logo.svg" alt="logo" /></a>
+		<span class="ml-[11.5px]">/</span>
+		<span class="text-xl font-semibold ml-[11.5px]">Proof Market</span>
 	</div>
 	<div class="flex-none mr-6">
 		<ConnectButton />
