61662ac57c675eeb5c8d649fde642a816a531d55
Souvik Mishra <mishra.souvik911@gmail.com>
2023-11-06 11:18:57 +0530

    add market info based on selected market

--- src/lib/components/market-header/HeaderPriceInfo.svelte
+++ src/lib/components/market-header/HeaderPriceInfo.svelte
@@ -1,24 +1,22 @@
 <script lang="ts">
+	import { selectedMarket } from '$lib/stores/general-data';
 	import PaddingBorder from '../common/PaddingBorder.svelte';
 	import HeaderPriceCell from './HeaderPriceCell.svelte';
-	import type { PairPriceInfo } from './types';
 
-	export let pairPriceInfo: PairPriceInfo;
+	$: priceByTime = $selectedMarket.avg_cost / ($selectedMarket.avg_time / 60);
 </script>
 
 <div class="ml-5 flex flex-row items-center gap-6">
-	<div class="text-2xl font-semibold text-base-content">
-		{pairPriceInfo.price}
-	</div>
-	<HeaderPriceCell data={`$ ${pairPriceInfo['priceByTimeUSD']}`} label={'Price/Time'} />
+	<div class="text-2xl font-semibold text-base-content">TODO</div>
+	<HeaderPriceCell data={`$ ${priceByTime.toFixed(2)}`} label={'Price/Time'} />
 	<PaddingBorder />
-	<HeaderPriceCell data={`$ ${pairPriceInfo['avgCostUSD']}`} label={'Avg. Cost'} />
+	<HeaderPriceCell data={`$ ${$selectedMarket.avg_cost}`} label={'Avg. Cost'} />
 	<PaddingBorder />
 	<HeaderPriceCell
-		data={`${(pairPriceInfo['avgTimeInMinutes'] / 60).toFixed(2)} hours`}
+		data={`${($selectedMarket.avg_time / 60).toFixed(2)} hours`}
 		label={'Avg. Time'}
 	/>
 	<PaddingBorder />
-	<HeaderPriceCell data={`${pairPriceInfo['proofsExecuted']}`} label={'Proofs Done'} />
+	<HeaderPriceCell data={`${$selectedMarket.total_proofs}`} label={'Proofs Done'} />
 	<PaddingBorder />
 </div>
--- src/lib/components/market-header/MarketInfoHeader.svelte
+++ src/lib/components/market-header/MarketInfoHeader.svelte
@@ -81,5 +81,5 @@
 	</TransparentDropdown>
 	<!-- {selMarket} -->
 	<div class="my-[10px] h-[calc(100%-20px)] w-[1px] bg-[#202740]" />
-	<HeaderPriceInfo pairPriceInfo={curPairPriceInfo} />
+	<HeaderPriceInfo />
 </div>
