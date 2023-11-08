78a2c34daa57feaadd00bf08ec2a07efeb56bbec
Souvik Mishra <mishra.souvik911@gmail.com>
2023-11-08 13:23:35 +0530

    update support for walletconnect

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,56 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import inject from '@rollup/plugin-inject';
 
-export default defineConfig({
-	plugins: [sveltekit()]
-});
+import type { UserConfig } from 'vite';
+import nodePolyfills from 'rollup-plugin-polyfill-node';
+
+// For further build env configs and troubleshooting
+// checkout our official docs [here](https://onboard.blocknative.com/docs/modules/core#sveltekit-vite)
+
+const MODE = 'prod';
+const development = MODE === 'development';
+
+/** @type {import('@sveltejs/kit').Config} */
+
+const config: UserConfig = {
+	plugins: [
+		sveltekit(),
+		development &&
+			nodePolyfills({
+				include: ['node_modules/**/*.js', new RegExp('node_modules/.vite/.*js'), 'http', 'process']
+			})
+	],
+	resolve: {
+		alias: {
+			process: 'process/browser',
+			crypto: 'crypto-browserify',
+			stream: 'stream-browserify',
+			assert: 'assert',
+			buffer: 'buffer'
+		}
+	},
+	build: {
+		rollupOptions: {
+			external: ['@web3-onboard/*'],
+			plugins: [
+				nodePolyfills({ include: ['crypto', 'http'] }),
+				inject({ Buffer: ['buffer', 'Buffer'] })
+			]
+		},
+		commonjsOptions: {
+			transformMixedEsModules: true
+		}
+	},
+	optimizeDeps: {
+		exclude: ['@ethersproject/hash', 'wrtc', 'http'],
+		include: ['@web3-onboard/core', 'js-sha3', '@ethersproject/bignumber'],
+		esbuildOptions: {
+			// Node.js global to browser globalThis
+			define: {
+				global: 'globalThis'
+			}
+		}
+	}
+};
+
+export default config;
