9c2eabc6d5b6d8dd697b3dd170e43beaff367e6a
Souvik Mishra <mishra.souvik911@gmail.com>
2023-11-08 14:09:15 +0530

    update market header info prices

--- src/lib/components/market-header/HeaderPriceInfo.svelte
+++ src/lib/components/market-header/HeaderPriceInfo.svelte
@@ -24,14 +24,16 @@
 
 <div class="ml-3 flex flex-row items-center gap-3 2xl:ml-5 2xl:gap-6">
 	<div class="text-lg font-semibold text-base-content 2xl:text-2xl">
-		{`$${parseInt($selectedMarket.total_value).toLocaleString('en-US', {
-			minimumFractionDigits: 2
+		{`$${(parseInt($selectedMarket.total_value) / 100000000).toLocaleString('en-US', {
+			minimumFractionDigits: 2,
+			maximumFractionDigits: 5
 		})}`}
 	</div>
 	<PaddingBorder />
 	<HeaderPriceCell
-		data={`${parseInt($selectedMarket.avg_cost).toLocaleString('en-US', {
-			minimumFractionDigits: 2
+		data={`${(parseInt($selectedMarket.avg_cost) / 100000000).toLocaleString('en-US', {
+			minimumFractionDigits: 2,
+			maximumFractionDigits: 5
 		})}`}
 		label={'Avg. Cost (USDC)'}
 	/>
